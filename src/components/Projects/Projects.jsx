0833d8d017475d6375bbaee23314368881987ee7
Hamid <hamidarianyegane@gmail.com>
2023-09-26 18:04:12 +0330

    Project component updated

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import stripe from "../../Assets/Projects/stripe.png";
+import cart from "../../Assets/Projects/cart.png";
+import todoList from "../../Assets/Projects/todoList.png";
+import flowerShop from "../../Assets/Projects/flowerShop.png";
+import menu from "../../Assets/Projects/menu.png";
+import cocktaildb from "../../Assets/Projects/CocktailDB.png";
+import palette from "../../Assets/Projects/palette.png";
+import slider from "../../Assets/Projects/slider.png";
 
 function Projects() {
   return (
@@ -23,67 +25,89 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={flowerShop}
               isBlog={false}
-              title="Chatify"
+              title="Flower Shop"
               description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-              ghLink="https://github.com/arianhamid"
-              demoLink="https://arianhamid.ir"
+              ghLink="https://github.com/arianhamid/Flowers-store-project"
+              demoLink="https://arian-flower-store.netlify.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={cocktaildb}
               isBlog={false}
-              title="Bits-0f-C0de"
+              title="CocktailDB"
               description="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-              ghLink="https://github.com/arianhamid"
-              demoLink="https://arianhamid.ir"
+              ghLink="https://github.com/arianhamid/cocktails"
+              demoLink="https://arian-cocktaildb.netlify.app"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={todoList}
               isBlog={false}
-              title="Editor.io"
+              title="Todo List"
               description="Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?"
-              ghLink="https://github.com/arianhamid"
-              demoLink="https://arianhamid.ir"
+              ghLink="https://github.com/arianhamid/todo-app-react"
+              demoLink="https://arian-todo-app.netlify.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={menu}
               isBlog={false}
-              title="Plant AI"
-              description="At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus."
-              ghLink="https://github.com/arianhamid"
-              demoLink="https://arianhamid.ir"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social media"
+              title="Filter Feature Menu"
               description="Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
-              ghLink="https://github.com/arianhamid"
-              // demoLink="https://www.youtube.com/watch?v=dQw4wgXcQ&ab_channel=RiAstley" <--------Please include a demo link here
+              ghLink="https://github.com/arianhamid/menu-app"
+              demoLink="https://arian-menu-filter.netlify.app/"
+            />
+          </Col>
+          
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={palette}
+              isBlog={false}
+              title="Color Generator"
+              description=" cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est omnis dolor repelle.Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet consectetur adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam."
+              ghLink="https://github.com/arianhamid/color-generator"
+              demoLink="https://arian-color-generator.netlify.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={slider}
               isBlog={false}
-              title="Face Recognition"
+              title="Slider App"
+              description="Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?"
+              ghLink="https://github.com/arianhamid/slider-app"
+              demoLink="https://arian-slider-app.netlify.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={stripe}
+              isBlog={false}
+              title="Stripe Sub-Menu Navbar"
+              description="At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore"
+              ghLink="https://github.com/arianhamid/stripe-submenus-app"
+              demoLink="https://arian-stripe-navbar.netlify.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={cart}
+              isBlog={false}
+              title="Shpping Cart"
               description="On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain."
-              ghLink="https://github.com/arianhamid"
-              // demoLink="https://arianhamid.ir/"      <--------Please include a demo link here
+              ghLink="https://github.com/arianhamid/cart"
+              demoLink="https://arian-cart.netlify.app/"
             />
           </Col>
         </Row>
