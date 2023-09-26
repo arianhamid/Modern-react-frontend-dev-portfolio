c6736aa3530342b330947ad447c8e52447875815
Hamid <hamidarianyegane@gmail.com>
2023-09-26 20:25:12 +0330

    home bg css updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,43 +8,41 @@ import Type from "./Type";
 function Home() {
   return (
     <section>
-      <div className="galaxy">
-        <Container fluid className="home-section" id="home">
-          <Particle />
-          <Container className="home-content">
-            <Row>
-              <Col md={7} className="home-header">
-                <h1 style={{ paddingBottom: 15 }} className="heading">
-                  Hi There!{" "}
-                  <span className="wave" role="img" aria-labelledby="wave">
-                    👋🏻
-                  </span>
-                </h1>
+      <Container fluid className="home-section" id="home">
+        <Particle />
+        <Container className="home-content">
+          <Row>
+            <Col md={7} className="home-header">
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+                Hi There!{" "}
+                <span className="wave" role="img" aria-labelledby="wave">
+                  👋🏻
+                </span>
+              </h1>
 
-                <h1 className="heading-name">
-                  I'M
-                  <strong className="main-name"> ArianYegane Hamid</strong>
-                </h1>
+              <h1 className="heading-name">
+                I'M
+                <strong className="main-name"> ArianYegane Hamid</strong>
+              </h1>
 
-                <div style={{ padding: 50, textAlign: "left" }}>
-                  <Type />
-                </div>
-              </Col>
+              <div style={{ padding: 50, textAlign: "left" }}>
+                <Type />
+              </div>
+            </Col>
 
-              <Col md={5} style={{ paddingBottom: 20 }}>
-                <object type="image/svg+xml" data={homeLogo}>
-                  <img
-                    src={homeLogo}
-                    alt="home pic"
-                    style={{ maxHeight: "450px" }}
-                  />
-                </object>
-              </Col>
-            </Row>
-          </Container>
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <object type="image/svg+xml" data={homeLogo}>
+                <img
+                  src={homeLogo}
+                  alt="home pic"
+                  style={{ maxHeight: "450px" }}
+                />
+              </object>
+            </Col>
+          </Row>
         </Container>
-        <Home2 />
-      </div>
+      </Container>
+      <Home2 />
     </section>
   );
 }
