e3211f3e630e5c881a21a42e6dec8690ded1696c
Hamid <hamidarianyegane@gmail.com>
2023-10-03 18:40:27 +0330

    resume tab added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -81,7 +81,7 @@ function NavBar() {
                 Projects
               </Nav.Link>
             </Nav.Item>
-{/* 
+
             <Nav.Item>
               <Nav.Link
                 as={Link}
@@ -91,7 +91,7 @@ function NavBar() {
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
               </Nav.Link>
             </Nav.Item>
-
+{/* 
             <Nav.Item>
               <Nav.Link
                 href="https://arianhamid.ir/"
